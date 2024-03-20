e9034f24570ace3b17709d1a9a2bfae4cd22d0cf
Gilvan Gabriel <gilvangabriealencar@gmail.com>
2024-03-19 20:52:05 -0400

    Gilvan Gabriel
    LeituraNegativos/LeituraDados

---
+++ LeituraDados/LeituraDados/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace LeituraDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            N = int.Parse(Console.ReadLine());
+
+            string[] nome = new string[N];
+            int[] idade = new int[N];
+            double[] altura = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                nome[i] = vet[0];
+                idade[i] = int.Parse(vet[1]);
+                altura[i] = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            }
+
+            double somaAlturas = 0;
+            double media;
+
+            for (int i = 0; i < N; i++)
+            {
+                somaAlturas += altura[i];
+            }
+
+            media = somaAlturas / N;
+
+            int contIdades = 0;
+            for (int i = 0;i < N; i++) 
+            {
+                if (idade[i] > 16) 
+                {
+                    contIdades++;
+                }
+            }
+
+            double porcentIdade = (double) contIdades / N * 100.0;
+
+            Console.WriteLine("Altura média: " + media.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Pessoas com menos de 16 anos: " + porcentIdade.ToString("F0", CultureInfo.InvariantCulture) + "%");
+        }
+    }
+}
---
+++ MediaVetor/MediaVetor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace MediaVetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            double[] vet;
+
+            N = int.Parse(Console.ReadLine());
+
+            vet = new double[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++)
+            {
+                vet[i] = double.Parse(s[i], CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0;i < N; i++)
+            {
+                Console.Write(vet[i].ToString("F1", CultureInfo.InvariantCulture) + " ");
+            }
+
+            Console.WriteLine();
+            double soma = 0, media;
+
+            for (int i = 0; i < N; i++) 
+            { 
+                soma = soma + vet[i];
+            }
+
+            Console.WriteLine(soma.ToString("F2", CultureInfo.InvariantCulture));
+            media = soma / N;
+
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
