f68db2ff850b3b0f75e7708ac222d2baa10a3ab8
Gilvan Gabriel <93411051+GilvanGabrielCA@users.noreply.github.com>
2024-03-21 10:47:04 -0400

    Exercicios membros estaticos

---
+++ ConversorMoedaPOO/ConversorMoedaPOO/ConversorDeMoedas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorMoedaPOO
+{
+    internal class ConversorDeMoedas
+    {
+        public static double Iof = 6.0;
+
+        public static double ValorPago(double quantia, double cotacao)
+        {
+            double total = quantia * cotacao;
+
+            return total + total * Iof / 100.0 ;
+        }
+
+    }
+}
