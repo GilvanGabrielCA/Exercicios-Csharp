df2ae87e7eab273a15232c231dd5cde9181b6923
Gilvan Gabriel <gilvangabriealencar@gmail.com>
2024-03-23 12:50:01 -0400

    gilvan g
    Estudandos Alugel

---
+++ AluguelQuartos/AluguelQuartos/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace AluguelQuartos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudantes[] vetor = new Estudantes[10]; 
+
+            Console.Write("Quantos quartos serao alugados? ");
+            int QuartosAlugados = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= QuartosAlugados; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Alugel #" + i + ":");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vetor[quarto] = new Estudantes(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vetor[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vetor[i]);
+                }
+            }
+        }
+    }
+}
